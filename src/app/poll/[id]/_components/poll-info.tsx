da16d8d030f6c7a753bd3e3756b93eb13f596ca6
Khalil <khalilpati55@gmail.com>
2024-09-12 14:15:51 +0530

    Created by details in poll info

--- src/app/poll/[id]/_components/poll-info.tsx
+++ src/app/poll/[id]/_components/poll-info.tsx
@@ -1,14 +1,21 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import { rings } from "@/lib/constants";
 import { createClient } from "@/lib/supabase/server";
 import { getRandomElement } from "@/lib/utils";
-import dynamic from "next/dynamic";
+import nextDynamic from "next/dynamic";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 import React from "react";
-const Timer = dynamic(() => import("../_components/timer"), { ssr: false });
+const Timer = nextDynamic(() => import("../_components/timer"), {
+  ssr: false,
+  loading: () => <Skeleton className="h-7 w-32"></Skeleton>,
+});
 
 type Props = {
   id: string;
 };
+
+export const dynamic = "force-static";
 
 const PollInfo = async ({ id }: Props) => {
   const { backgroundColor, ringColor } = getRandomElement(rings);
@@ -16,7 +23,7 @@ const PollInfo = async ({ id }: Props) => {
   const supabase = createClient();
   const { data, error } = await supabase
     .from("poll")
-    .select("title, description, ends_at")
+    .select("title, description, ends_at, user:users(name, avatar_url)")
     .eq("id", id)
     .single();
 
@@ -27,8 +34,25 @@ const PollInfo = async ({ id }: Props) => {
   return (
     <div className="group relative h-full">
       <div className="flex min-h-40 flex-col gap-3 rounded-md border bg-slate-50 p-5 dark:border-zinc-600 dark:bg-zinc-800">
+        {data.user && (
+          <div className="flex items-center gap-3">
+            <Image
+              src={data.user.avatar_url}
+              alt={data.user.name}
+              width={30}
+              height={30}
+              className={`rounded-full ring-2`}
+              style={{ "--tw-ring-color": ringColor } as React.CSSProperties}
+            />
+            <div>
+              <h1 className="text-base">{data.user.name}</h1>
+            </div>
+          </div>
+        )}
         <h1 className="break-words text-3xl font-bold">{data.title}</h1>
-        <p className="text-xl">{data.description}</p>
+        <p className="text-lg text-gray-700 dark:text-gray-300">
+          {data.description}
+        </p>
         <Timer date={data.ends_at} />
       </div>
       <div
